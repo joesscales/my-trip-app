92ac7393e69a1ea4d527bffcc9b61c182e0609bd
joesscales <joesscales@gmail.com>
2024-07-03 07:52:23 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'my-trip-app',
+  webDir: 'www'
+};
+
+export default config;
